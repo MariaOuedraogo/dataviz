165838d49c9506d344cbb7ea7b213d340cca8db8
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2023-11-09 17:09:40 +0100

    :tada:
    création de la liste en ordre croissant des formes d'exploitation.

--- list.js
+++ list.js
@@ -1,52 +1,67 @@
-d3.json( "list.json" ).then(function(data) {
+// cette association permettra d'avoir des noms communs pour lors de l'affichage sur écran //
+const names = {
+  "typeOfLabourConstruction": "construction",
+    "typeOfLabourDomesticWork": "travail domestique",
+    "typeOfLabourHospitality": "typeOfLabourHospitality blabla ",
+    "typeOfLabourOther": "autres formes de travail forcé",
+    "typeOfSexProstitution": "prostitution ",
+    "typeOfSexPornography": "Sexe et pornographie",
+    "typeOfSexOther": "autre forme d'exploitation sexuelle"
+}
 
-    let largeur_colonne=50/data.length;
+//début du traitement en json//
+fetch('list.json').then(function (response) {
+  response.json().then(function (data) {
+
+    // // j'initialise la variable resultat_etape_1
+    // let resultat_etape_1 = 0
+
+    // console.log(resultat_etape_1)
+    // j'initialise la constante total afin de créer l'objet qui contiendra les objets avec le nombre total de fr bictime par type d'exploitation//
+    const total = {
+      //je les initialise à zéro//
+    "typeOfLabourConstruction": 0,
+    "typeOfLabourDomesticWork": 0,
+    "typeOfLabourHospitality": 0,
+    "typeOfLabourOther": 0,
+    "typeOfSexProstitution": 0,
+    "typeOfSexPornography": 0,
+    "typeOfSexOther": 0
+    }
+      //après avoir récupérer le nombre pour une tranche d'âge , la boucle ajoute le résultat présent dans la l'objet suivant//
+      data.forEach(function (tranche_age_obj) {
+        total.typeOfLabourConstruction += tranche_age_obj.typeOfLabourConstruction
+        total.typeOfLabourDomesticWork += tranche_age_obj.typeOfLabourDomesticWork
+        total.typeOfLabourHospitality += tranche_age_obj.typeOfLabourHospitality
+        total.typeOfLabourOther += tranche_age_obj.typeOfLabourOther
+        total.typeOfSexProstitution += tranche_age_obj.typeOfSexProstitution
+        total.typeOfSexPornography += tranche_age_obj.typeOfSexPornography
+        total.typeOfSexOther += tranche_age_obj.typeOfSexOther
 
 
-
-    d3.select("svg")
-        .selectAll(".histobarre")
-        .data(data)
-        .join("g")
-        .attr("class","histobarre")
-        .attr("transform", (d,i)=>`translate(0,${i*largeur_colonne})`);
-    
-    d3.selectAll(".histobarre")
-        .append("rect")
-        .attr("width", d=> d["valeur"]*40/17000 )
-        .attr("height",largeur_colonne/2)
-        .attr("transform", `translate(0,${largeur_colonne/2})`)
-        .style("fill","red");
-    
-    
-    d3.selectAll(".histobarre")
-        .append("text")
-        .text(d=>d.type)
-        .style("fill","blue")
-        .attr("transform", `translate(0,${largeur_colonne/2})`)
-        // changer la taille du texte 
-        .style("font-size","0.1rem") 
-        
-
-
-    d3.selectAll(".histobarre")
-        .on("mouseenter", function(e,d){
-        // transparence 
-        d3.selectAll(".histobarre")
-            .style("opacity","0.5")
-        d3.select(this)
-            .style("opacity",null)
     })
+
+
+    //le trie//
+
+    const entries = Object.entries(total)
+
+    const finalData = entries.map(([key, valueTotal]) => {
+      return [names[key], valueTotal]
+    }).sort((a, b) => b[1] - a[1])
+    console.log(finalData)
+
     
-    d3.selectAll(".histobarre")
-        .on("mouseleave", function(){
-        //  transparence 
+    // console.log(total)
+
+   let classement = document.querySelector(".classement");
+
+      classement.innerHTML += "<li>"+ finalData + "</li>"
     
-        d3.selectAll(".histobarre")
-            .style("opacity",null)
-    
-    })
-    
- });
+  })
+})
+
+
+
 
 
